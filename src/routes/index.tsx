3da7e475400200bf701c0542a103402d5650e042
francismiko <1485059943@qq.com>
2024-05-10 02:55:32 +0800

    feat: textarea rows signal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useSignal } from '@builder.io/qwik'
 
 export default component$(() => {
+	const rows = useSignal<number>(1)
+
 	return (
-		<div class="flex flex-col w-full h-ful">
-			<div class="h-[90vh] overflow-y-auto">
-				<div class="h-[200vh] mx-[15vw]"></div>
+		<div class="flex flex-col w-full max-h-full">
+			<div class="flex-1  overflow-y-auto">
+				<div class="h-[200vh] mx-[15vw]">123</div>
 			</div>
-			<div class="h-[10vh]">
-				<input class="w-full" />
+			<div class="min-h-[10vh] w-[100vw]">
+				<textarea
+					onInput$={({ target }) => {
+						rows.value = (target as HTMLTextAreaElement).value.split(
+							'\n',
+						).length
+					}}
+					rows={rows.value}
+					class="block resize-none w-[50vw] mx-auto max-h-[25vh] h-auto border"
+				/>
 			</div>
 		</div>
 	)
