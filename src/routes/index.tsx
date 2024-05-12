815608e6f3cad92d27d3b10732b435886a57af1e
francismiko <1485059943@qq.com>
2024-05-12 18:55:16 +0800

    feat: input handler

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,27 @@
-import { component$, useSignal } from '@builder.io/qwik'
+import { $, component$, useSignal } from '@builder.io/qwik'
+import { isEmpty, trim } from 'radash'
 
 export default component$(() => {
+	const textareaRef = useSignal<HTMLTextAreaElement>()
 	const rows = useSignal<number>(1)
+	const text = useSignal<string>('')
+
+	const handleInput = $(() => {
+		if (!textareaRef.value) return
+		rows.value = textareaRef.value.value.split('\n').length
+		text.value = textareaRef.value.value
+		console.log(123)
+	})
+
+	const handleKeydown = $((event: KeyboardEvent) => {
+		if (event.key === 'Enter') {
+			if (isEmpty(trim(text.value, '\n ')) || !textareaRef.value) return
+			event.preventDefault()
+			console.log(text.value)
+			text.value = ''
+			rows.value = 1
+		}
+	})
 
 	return (
 		<div class="flex flex-col w-full max-h-full">
@@ -10,12 +30,11 @@ export default component$(() => {
 			</div>
 			<div class="min-h-[10vh] w-[100vw]">
 				<textarea
-					onInput$={({ target }) => {
-						rows.value = (target as HTMLTextAreaElement).value.split(
-							'\n',
-						).length
-					}}
+					ref={textareaRef}
+					onInput$={handleInput}
+					onKeyDown$={handleKeydown}
 					rows={rows.value}
+					value={text.value}
 					class="block resize-none w-[50vw] mx-auto max-h-[25vh] h-auto border"
 				/>
 			</div>
